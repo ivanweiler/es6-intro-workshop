55cd191355bfe20376f0f40b3076dd7778b633f5
Ivan Weiler <ivan.weiler@inchoo.net>
2018-05-07 13:13:09 +0200

    Backend

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'stage-2']
+          }
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    publicPath: "/dist/"
+  }
+
+};
